95f1576a6d062d7eada69d696bbd5821c6c00a28
Matthew Garcia <matt.e.garcia@gmail.com>
2025-07-24 15:43:31 -0500

    specify static methods in Snow class

--- src/Snow.cs
+++ src/Snow.cs
@@ -9,7 +9,7 @@ namespace Landis.Library.PnETCohorts
         /// </summary>
         /// <param name="daysOfWinter"></param>
         /// <returns></returns>
-        public float CalcDensity(int daysOfWinter)
+        public static float CalcDensity(int daysOfWinter)
         {
             float densitySnow_kg_m3 = Constants.DensitySnow_intercept + (Constants.DensitySnow_slope * daysOfWinter);
             return densitySnow_kg_m3;
@@ -21,7 +21,7 @@ namespace Landis.Library.PnETCohorts
         /// <param name="daysOfWinter"></param>
         /// <param name="snowpack"></param>
         /// <returns></returns>
-        public float CalcDepth(float densitySnow_kg_m3, float snowpack)
+        public static float CalcDepth(float densitySnow_kg_m3, float snowpack)
         {
             float depthSnow = Constants.DensityWater * snowpack / densitySnow_kg_m3 / 1000;
             return depthSnow;
@@ -35,7 +35,7 @@ namespace Landis.Library.PnETCohorts
         /// </summary>
         /// <param name="densitySnow_kg_m3"></param>
         /// <returns></returns>
-        public float CalcThermalConductivity(float densitySnow_kg_m3)
+        public static float CalcThermalConductivity(float densitySnow_kg_m3)
         {
             float thermalConductivity = (float)(Constants.ThermalConductivityAir_Watts + ((0.0000775 * densitySnow_kg_m3) + (0.000001105 * Math.Pow(densitySnow_kg_m3, 2))) * (Constants.ThermalConductivityIce_Watts - Constants.ThermalConductivityAir_Watts)) * 3.6F * 24F;
             return thermalConductivity;
@@ -48,7 +48,7 @@ namespace Landis.Library.PnETCohorts
         /// </summary>
         /// <param name="thermalConductivity_Snow"></param>
         /// <returns></returns>
-        public float CalcThermalDamping(float thermalConductivity_Snow)
+        public static float CalcThermalDamping(float thermalConductivity_Snow)
         {
             float thermalDamping = (float)Math.Sqrt(Constants.omega / (2.0F * thermalConductivity_Snow));
             return thermalDamping;
@@ -63,7 +63,7 @@ namespace Landis.Library.PnETCohorts
         /// <param name="snowDepth"></param>
         /// <param name="thermalDamping"></param>
         /// <returns></returns>
-        public float CalcDampingRatio(float snowDepth, float thermalDamping)
+        public static float CalcDampingRatio(float snowDepth, float thermalDamping)
         {
             float dampingRatio = (float)Math.Exp(-1.0F * snowDepth * thermalDamping);
             return dampingRatio;

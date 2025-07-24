a0a4de0648e6aafec0e801a906a3243c89c7f849
Matthew Garcia <matt.e.garcia@gmail.com>
2025-07-24 15:29:04 -0500

    new Snow class with methods taken from Permafrost.cs

--- src/Permafrost.cs
+++ src/Permafrost.cs
@@ -8,20 +8,18 @@ namespace Landis.Library.PnETCohorts
     {
         public static SortedList<float, float> CalcMonthlySoilTemps(SortedList<float, float> depthTempDict, IEcoregionPnET Ecoregion, int daysOfWinter, float snowpack, IHydrology hydrology, float lastTempBelowSnow)
         {
-            float[] snowResults = CalcSnowDepth(daysOfWinter, snowpack);
-            float snowDepth = snowResults[0];
-            float DensitySnow_kg_m3 = snowResults[1];
+            // Snow calculations, now handled in Snow class
+            float densitySnow_kg_m3 = Snow.CalcDensity(daysOfWinter);
+            float snowDepth = Snow.CalcDepth(densitySnow_kg_m3, snowpack);
             if (Ecoregion.Variables.Tavg >= 0)
             {
                 float fracAbove0 = Ecoregion.Variables.Tmax / (Ecoregion.Variables.Tmax - Ecoregion.Variables.Tmin);
                 snowDepth = snowDepth * fracAbove0;
             }
-            // based on CLM model - https://escomp.github.io/ctsm-docs/doc/build/html/tech_note/Soil_Snow_Temperatures/CLM50_Tech_Note_Soil_Snow_Temperatures.html#soil-and-snow-thermal-properties
-            // Eq. 85 in Jordan (1991)
-            // Damping ratio for snow - adapted from Kang et al. (2000) and Liang et al. (2014)
-            float snowThermalDamping = CalcSnowThermalDamping(DensitySnow_kg_m3);
-            float DRz_snow = (float)Math.Exp(-1.0F * snowDepth * snowThermalDamping); 
-            // Frozen soil calculations - from "Soil thawing worksheet.xlsx"
+            float snowThermalConductivity = Snow.CalcThermalConductivity(densitySnow_kg_m3);
+            float snowThermalDamping = Snow.CalcThermalDamping(snowThermalConductivity);
+            float snowDampingRatio = Snow.CalcDampingRatio(snowDepth, snowThermalDamping); 
+            // Frozen soil calculations
             float soilPorosity = Ecoregion.Porosity / Ecoregion.RootingDepth;  //m3/m3
             float soilWaterContent = hydrology.SoilWaterContent / Ecoregion.RootingDepth;  //m3/m3
             float ga = 0.035F + 0.298F * (soilWaterContent / soilPorosity);
@@ -33,12 +31,13 @@ namespace Landis.Library.PnETCohorts
             float Dmonth = D * Ecoregion.Variables.DaySpan; // m2/month
             float ks = Dmonth * 1000000F / (Ecoregion.Variables.DaySpan * Constants.SecondsPerDay); // mm2/s
             float d = (float)Math.Pow(Constants.omega / (2.0F * Dmonth), 0.5);
+            // 
             float maxDepth = Ecoregion.RootingDepth + Ecoregion.LeakageFrostDepth;
             float freezeDepth = maxDepth;
             float testDepth = 0;
             float tempBelowSnow = Ecoregion.Variables.Tavg;
             if (snowDepth > 0)
-                tempBelowSnow = lastTempBelowSnow + (Ecoregion.Variables.Tavg - lastTempBelowSnow) * DRz_snow;
+                tempBelowSnow = lastTempBelowSnow + (Ecoregion.Variables.Tavg - lastTempBelowSnow) * snowDampingRatio;
             lastTempBelowSnow = tempBelowSnow;
             while (testDepth <= (maxDepth / 1000.0))
             {
@@ -55,38 +54,6 @@ namespace Landis.Library.PnETCohorts
             if (maxDepth < 100) // mm
                 depthTempDict[0.1F] = depthTempDict[0];
             return depthTempDict;
-        }
-
-        /// <summary>
-        /// Calculate snow density (kg/m3, function of daysOfWinter) 
-        /// and snow depth (m)
-        /// </summary>
-        /// <param name="daysOfWinter"></param>
-        /// <param name="snowpack"></param>
-        /// <returns></returns>
-        public static float[] CalcSnowDepth(int daysOfWinter, float snowpack)
-        {
-            float DensitySnow_kg_m3 = Constants.DensitySnow_intercept + (Constants.DensitySnow_slope * daysOfWinter);
-            float snowDepth = Constants.DensityWater * snowpack / DensitySnow_kg_m3 / 1000;
-            float[] snowArray = new float[2];
-            snowArray[0] = snowDepth;
-            snowArray[1] = DensitySnow_kg_m3;
-            return snowArray;
-        }
-
-        /// <summary>
-        /// Calculate snow thermal damping coefficient
-        /// based on CLM model - https://escomp.github.io/ctsm-docs/doc/build/html/tech_note/Soil_Snow_Temperatures/CLM50_Tech_Note_Soil_Snow_Temperatures.html#soil-and-snow-thermal-properties
-        /// Eq. 85 in Jordan (1991)
-        /// ThermalConductivity_Snow (kJ/m.d.K) includes unit conversion from W to kJ
-        /// </summary>
-        /// <param name="DensitySnow_kg_m3"></param>
-        /// <returns></returns>
-        public static float CalcSnowThermalDamping(float DensitySnow_kg_m3)
-        {
-            float ThermalConductivity_Snow = (float)(Constants.ThermalConductivityAir_Watts + ((0.0000775 * DensitySnow_kg_m3) + (0.000001105 * Math.Pow(DensitySnow_kg_m3, 2))) * (Constants.ThermalConductivityIce_Watts - Constants.ThermalConductivityAir_Watts)) * 3.6F * 24F;
-            float SnowThermalDamping = (float)Math.Sqrt(Constants.omega / (2.0F * ThermalConductivity_Snow));
-            return SnowThermalDamping;
         }
     }
 }
---
+++ src/Snow.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Landis.Library.PnETCohorts
+{
+    public class Snow
+    {
+        /// <summary>
+        /// Calculate snow density (kg/m3, function of daysOfWinter) 
+        /// </summary>
+        /// <param name="daysOfWinter"></param>
+        /// <returns></returns>
+        public float CalcDensity(int daysOfWinter)
+        {
+            float densitySnow_kg_m3 = Constants.DensitySnow_intercept + (Constants.DensitySnow_slope * daysOfWinter);
+            return densitySnow_kg_m3;
+        }
+
+        /// <summary>
+        /// Calculate snow depth (m)
+        /// </summary>
+        /// <param name="daysOfWinter"></param>
+        /// <param name="snowpack"></param>
+        /// <returns></returns>
+        public float CalcDepth(float densitySnow_kg_m3, float snowpack)
+        {
+            float depthSnow = Constants.DensityWater * snowpack / densitySnow_kg_m3 / 1000;
+            return depthSnow;
+        }
+
+        /// <summary>
+        /// Calculate thermal conductivity of snow (kJ/m.d.K) 
+        /// includes unit conversion from W to kJ
+        /// based on CLM model - https://escomp.github.io/ctsm-docs/doc/build/html/tech_note/Soil_Snow_Temperatures/CLM50_Tech_Note_Soil_Snow_Temperatures.html#soil-and-snow-thermal-properties
+        /// Eq. 85 in Jordan (1991)
+        /// </summary>
+        /// <param name="densitySnow_kg_m3"></param>
+        /// <returns></returns>
+        public float CalcThermalConductivity(float densitySnow_kg_m3)
+        {
+            float thermalConductivity = (float)(Constants.ThermalConductivityAir_Watts + ((0.0000775 * densitySnow_kg_m3) + (0.000001105 * Math.Pow(densitySnow_kg_m3, 2))) * (Constants.ThermalConductivityIce_Watts - Constants.ThermalConductivityAir_Watts)) * 3.6F * 24F;
+            return thermalConductivity;
+        }
+
+        /// <summary>
+        /// Calculate snow thermal damping coefficient
+        /// based on CLM model - https://escomp.github.io/ctsm-docs/doc/build/html/tech_note/Soil_Snow_Temperatures/CLM50_Tech_Note_Soil_Snow_Temperatures.html#soil-and-snow-thermal-properties
+        /// Eq. 85 in Jordan (1991)
+        /// </summary>
+        /// <param name="thermalConductivity_Snow"></param>
+        /// <returns></returns>
+        public float CalcThermalDamping(float thermalConductivity_Snow)
+        {
+            float thermalDamping = (float)Math.Sqrt(Constants.omega / (2.0F * thermalConductivity_Snow));
+            return thermalDamping;
+        }
+
+        /// <summary>
+        /// Damping ratio for snow
+        /// adapted from Kang et al. (2000) and Liang et al. (2014)
+        /// based on CLM model - https://escomp.github.io/ctsm-docs/doc/build/html/tech_note/Soil_Snow_Temperatures/CLM50_Tech_Note_Soil_Snow_Temperatures.html#soil-and-snow-thermal-properties
+        /// Eq. 85 in Jordan (1991)
+        /// </summary>
+        /// <param name="snowDepth"></param>
+        /// <param name="thermalDamping"></param>
+        /// <returns></returns>
+        public float CalcDampingRatio(float snowDepth, float thermalDamping)
+        {
+            float dampingRatio = (float)Math.Exp(-1.0F * snowDepth * thermalDamping);
+            return dampingRatio;
+        }
+    }
+}

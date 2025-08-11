f5f1644f5ce7f3071364d2379db8c21b816af178
Matthew Garcia <matt.e.garcia@gmail.com>
2025-08-11 14:00:17 -0500

    corrected "Months" variable declaration

--- src/utility/Constants.cs
+++ src/utility/Constants.cs
@@ -39,20 +39,20 @@ namespace Landis.Library.PnETCohorts
         /// </summary>
         public const int billion = 1000000000;
 
-        public const enum Months
+        enum Months : int
         {
             January = 1,
-            February,
-            March,
-            April,
-            May,
-            June,
-            July,
-            August,
-            September,
-            October,
-            November,
-            December
+            February = 2,
+            March = 3,
+            April = 4,
+            May = 5,
+            June = 6,
+            July = 7,
+            August = 8,
+            September = 9,
+            October = 10,
+            November = 11,
+            December = 12
         }
 
         /// <summary>

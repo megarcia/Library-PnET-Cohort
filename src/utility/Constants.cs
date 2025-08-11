0abe64c1f32b2b0e252c9567e92575e56e0e597e
Matthew Garcia <matt.e.garcia@gmail.com>
2025-08-11 10:22:32 -0500

    moved several physical constants from Hydrology to Constants

--- src/utility/Constants.cs
+++ src/utility/Constants.cs
@@ -1,15 +1,16 @@
-﻿
+﻿using System;
+
 namespace Landis.Library.PnETCohorts 
 {
     public class Constants
     {
         /// <summary>
-        /// Molecular weight of C
+        /// Molecular weight of C in g/mol
         /// </summary>
         public const float MC = 12F;
 
         /// <summary>
-        /// Molecular weight of CO2
+        /// Molecular weight of CO2 in g/mol
         /// </summary>
         public const float MCO2 = 44F;
 
@@ -19,7 +20,7 @@ namespace Landis.Library.PnETCohorts
         public const float MCO2_MC = MCO2 / MC;
 
         /// <summary>
-        /// Reference atmospheric concentration of CO2 (ppm) )
+        /// Reference atmospheric concentration of CO2 in ppm
         /// </summary>
         public const float CO2RefConc = 350F;
 
@@ -55,14 +56,19 @@ namespace Landis.Library.PnETCohorts
         }
 
         /// <summary>
+        /// latent heat of vaporization for water in MJ/m3
+        /// </summary>
+        public const float LatentHeatVaporWater = 2453F;
+
+        /// <summary>
         /// heat capacity of solid portion of soil in kJ/m3.K 
-        /// (Farouki 1986 in vanLier and Durigon 2013)
+        /// (Farouki, 1986, in vanLier and Durigon, 2013)
         /// </summary>
         public const float HeatCapacitySoil = 1942F;
 
         /// <summary>
         /// heat capacity of water in kJ/m3.K
-        /// (vanLier and Durigon 2013)
+        /// (vanLier and Durigon, 2013)
         /// </summary>
         public const float HeatCapacityWater = 4186F;
 
@@ -74,50 +80,50 @@ namespace Landis.Library.PnETCohorts
 
         /// <summary>
         /// heat capacity of moss in kJ/m3.K 
-        /// (Sazonova and Romanovsky 2003)
+        /// (Sazonova and Romanovsky, 2003)
         /// </summary>
         public const float HeatCapacityMoss = 2500F;
 
         /// <summary>
         /// thermal conductivity of air in kJ/m.d.K 
-        /// (vanLier and Durigon 2013)
+        /// (vanLier and Durigon, 2013)
         /// </summary>
         public const float ThermalConductivityAir_kJperday = 2.25F;
 
         /// <summary>
         /// thermal conductivity of water in kJ/m.d.K
-        /// (vanLier and Durigon 2013)
+        /// (vanLier and Durigon, 2013)
         /// </summary>
         public const float ThermalConductivityWater_kJperday = 51.51F;
 
         /// <summary>
         /// thermal conductivity of clay soil in kJ/m.d.K
-        /// (Michot et al. 2008 in vanLier and Durigon 2013)
+        /// (Michot et al., 2008 in vanLier and Durigon, 2013)
         /// </summary>
         public const float ThermalConductivityClay = 80F;
 
         /// <summary>
         /// thermal conductivity of sandstone in kJ/m.d.K 
-        /// (Gemant 1950 in vanLier and Durigon 2013)
+        /// (Gemant, 1950, in vanLier and Durigon, 2013)
         /// </summary>
         public const float ThermalConductivitySandstone = 360F;
 
         /// <summary>
         /// thermal conductivity of moss in kJ/m.d.K
-        /// converted from 0.2 W/m.K (Sazonova and Romanovsky 2003)
+        /// converted from 0.2 W/m.K (Sazonova and Romanovsky, 2003)
         /// </summary>
         public const float ThermalConductivityMoss = 432F;
 
         /// <summary>
         /// unexplained coefficient in vanLier and Durigon (2013)
-        /// (via Farouki 1986)
+        /// (via Farouki, 1986)
         /// </summary>
         public const float gs = 0.125F;
 
         /// <summary>
         /// angular velocity of Earth in radians/month
         /// </summary>
-        public const float omega = (float)System.Math.PI * 2 / 12F;
+        public const float omega = (float)Math.PI * 2 / 12F;
 
         /// <summary>
         /// length of temp record in months
@@ -155,7 +161,7 @@ namespace Landis.Library.PnETCohorts
         /// minimum depth of snow (m) that counts as 
         /// full snow cover for albedo calculations
         /// </summary>
-        public const float snowReflectanceThreshold = 0.1F;  
+        public const float snowReflectanceThreshold = 0.1F;
 
         /// <summary>
         /// thermal diffusivity of moss
@@ -166,5 +172,32 @@ namespace Landis.Library.PnETCohorts
         /// thermal damping coefficient for moss
         /// </summary>
         public const float ThermalDampingMoss = (float)Math.Sqrt(2.0F * ThermalDiffusivityMoss / omega);
+
+        /// <summary>
+        /// Psychrometric coefficient in kPa/K
+        /// (Cabrera et al., 2016, Table 1)
+        /// </summary>
+        public const float PsychrometricCoeff = 0.062F;
+
+        /// <summary>
+        /// field capacity pore water (negative) pressure in kPa
+        /// </summary>
+        public const float FieldCapacity_kPa = -33F;
+
+        /// <summary>
+        /// field capacity pore water (negative) pressure in mmH2O
+        /// </summary>
+        public const float FieldCapacity_mmH2O = FieldCapacity_kPa / 9.804139432F;
+
+        /// <summary>
+        /// wilting point pore water (negative) pressure in kPa
+        /// </summary>
+        public const float WiltingPoint_kPa = -1500F;
+
+        /// <summary>
+        /// wilting point pore water (negative) pressure in mmH2O
+        /// </summary>
+        public const float WiltingPoint_mmH2O = WiltingPoint_kPa / 9.804139432F;
+
     }
 }

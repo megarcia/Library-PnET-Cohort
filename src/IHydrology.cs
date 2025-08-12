7e1ad4ec2995196d02590731f8da36a90874990d
Matthew Garcia <matt.e.garcia@gmail.com>
2025-08-12 15:21:48 -0500

    moved interface reference to CalcReferenceET_Hamon function to new IEvapotranspiration interface

--- src/IHydrology.cs
+++ src/IHydrology.cs
@@ -50,14 +50,6 @@ namespace Landis.Library.PnETCohorts
         bool SetFrozenSoilDepth(float depth); 
 
         /// <summary>
-        /// Calculate reference ET
-        /// </summary>
-        /// <param name="T"></param>
-        /// <param name="dayLength"></param>
-        /// <returns></returns>
-        float CalcReferenceET_Hamon(float T, float dayLength); 
-
-        /// <summary>
         /// Get the PressureHeadTable object
         /// </summary>
         Hydrology_SaxtonRawls PressureHeadTable { get; } 

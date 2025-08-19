2172646c97f2fa90b996379582ebfee247755798
Matthew Garcia <matt.e.garcia@gmail.com>
2025-08-19 14:03:47 -0500

    fixed a couple of missing parameters for CalcLAISum, removed unused versions of CalcLAI function

--- src/Canopy.cs
+++ src/Canopy.cs
@@ -4,8 +4,7 @@ namespace Landis.Library.PnETCohorts
 {
     public class Canopy
     {
-
-        public float CalcLAISum(float[] LAI)
+        public static float CalcLAISum(int index, float[] LAI)
         {
             float LAISum = 0;
             if (LAI != null)
@@ -15,25 +14,10 @@ namespace Landis.Library.PnETCohorts
                     LAISum += LAI[i];
                 }
             }
+            return LAISum;
         }
 
-        public float CalcLAI(IPnETSpecies species, float fol, int index)
-        {
-            // Leaf area index for the subcanopy layer by index. Function of specific leaf weight SLWMAX and the depth of the canopy
-            // Depth of the canopy is expressed by the mass of foliage above this subcanopy layer (i.e. slwdel * index/imax *fol)
-            float LAIlayerMax = (float)Math.Max(0.01, 25.0F - LAISum); // Cohort LAI is capped at 25; once LAI reaches 25 subsequent sublayers get LAI of 0.01
-            float LAIlayer = 1 / (float)Globals.IMAX * fol / (species.SLWmax - species.SLWDel * index * (1 / (float)Globals.IMAX) * fol);
-            if (fol > 0 && LAIlayer <= 0)
-            {
-                Globals.ModelCore.UI.WriteLine("\n Warning: LAI was calculated to be negative for " + species.Name + ". This could be caused by a low value for SLWmax.  LAI applied in this case is a max of 25 for each cohort.");
-                LAIlayer = LAIlayerMax / (Globals.IMAX - index);
-            }
-            else
-                LAIlayer = (float)Math.Min(LAIlayerMax, LAIlayer);
-            return LAIlayer;
-        }
-
-        public float CalcLAI(IPnETSpecies species, float fol, int index, float LAISum)
+        public static float CalcLAI(IPnETSpecies species, float fol, int index, float LAISum)
         {
             // Leaf area index for the subcanopy layer by index. Function of specific leaf weight SLWMAX and the depth of the canopy
             // Depth of the canopy is expressed by the mass of foliage above this subcanopy layer (i.e. slwdel * index/imax *fol)
--- src/Cohort.cs
+++ src/Cohort.cs
@@ -887,7 +887,7 @@ namespace Landis.Library.PnETCohorts
             float cohortIdealFol = PnETspecies.FolBiomassFrac_intercept * FActiveBiom * data.TotalBiomass;
             for (int i = 0; i < Globals.IMAX; i++)
             {
-                float LAISum = Canopy.CalcLAISum(LAI);
+                float LAISum = Canopy.CalcLAISum(i, LAI);
                 float subLayerLAI = Canopy.CalcLAI(PnETSpecies, cohortIdealFol, i, LAISum);
                 cohortLAI += subLayerLAI;
                 if (IsLeafOn)
@@ -941,7 +941,7 @@ namespace Landis.Library.PnETCohorts
             float cohortIdealFol = adjFolBiomassFrac * FActiveBiom * data.TotalBiomass;
             for (int i = 0; i < Globals.IMAX; i++)
             {
-                float LAISum = Canopy.CalcLAISum(LAI);
+                float LAISum = Canopy.CalcLAISum(i, LAI);
                 float subLayerLAI = Canopy.CalcLAI(PnETSpecies, cohortIdealFol, i, LAISum);
                 cohortLAI += subLayerLAI;
                 if (IsLeafOn)
@@ -1004,7 +1004,7 @@ namespace Landis.Library.PnETCohorts
             fOzone = 0;
             // Leaf area index for the subcanopy layer by index. Function of specific leaf weight SLWMAX and the depth of the canopy
             // Depth of the canopy is expressed by the mass of foliage above this subcanopy layer (i.e. slwdel * index/imax *fol)
-            float LAISum = Canopy.CalcLAISum(LAI);
+            float LAISum = Canopy.CalcLAISum(index, LAI);
             data.LAI[index] = Canopy.CalcLAI(PnETspecies, data.Fol, index, LAISum);
             if (MeltInByCanopyLayer > 0)
             {
@@ -1242,7 +1242,8 @@ namespace Landis.Library.PnETCohorts
                 }
             }
             // Leaf area index for the subcanopy layer by index. Function of specific leaf weight SLWMAX and the depth of the canopy
-            data.LAI[index] = Canopy.CalcLAI(PnETspecies, Fol, index, LAI);
+            LAISum = Canopy.CalcLAISum(index, LAI);
+            data.LAI[index] = Canopy.CalcLAI(PnETspecies, Fol, index, LAISum);
             // Adjust HalfSat for CO2 effect
             float halfSatIntercept = PnETspecies.HalfSat - Constants.CO2RefConc * PnETspecies.HalfSatFCO2;
             data.AdjHalfSat = PnETspecies.HalfSatFCO2 * variables.CO2 + halfSatIntercept;
@@ -1655,48 +1656,6 @@ namespace Landis.Library.PnETCohorts
             data.MaxBiomass = Math.Max(data.MaxBiomass, data.TotalBiomass);
             Fol *= (float)(1.0 - reductionFrac);
             data.MaxFolYear = Math.Max(data.MaxFolYear, Fol);
-        }
-
-        public float CalcLAI(IPnETSpecies species, float fol, int index)
-        {
-            // Leaf area index for the subcanopy layer by index. Function of specific leaf weight SLWMAX and the depth of the canopy
-            // Depth of the canopy is expressed by the mass of foliage above this subcanopy layer (i.e. slwdel * index/imax *fol)
-            float LAISum = 0;
-            if (LAI != null)
-            {
-                for (int i = 0; i < index; i++)
-                {
-                    LAISum += LAI[i];
-                }
-            }
-            float LAIlayerMax = (float)Math.Max(0.01, 25.0F - LAISum); // Cohort LAI is capped at 25; once LAI reaches 25 subsequent sublayers get LAI of 0.01
-            float LAIlayer = 1 / (float)Globals.IMAX * fol / (species.SLWmax - species.SLWDel * index * (1 / (float)Globals.IMAX) * fol);
-            if (fol > 0 && LAIlayer <= 0)
-            {
-                Globals.ModelCore.UI.WriteLine("\n Warning: LAI was calculated to be negative for " + species.Name + ". This could be caused by a low value for SLWmax.  LAI applied in this case is a max of 25 for each cohort.");
-                LAIlayer = LAIlayerMax / (Globals.IMAX - index);
-            }
-            else
-                LAIlayer = (float)Math.Min(LAIlayerMax, LAIlayer);
-            return LAIlayer;
-        }
-
-        public float CalcLAI(IPnETSpecies species, float fol, int index, float cumulativeLAI)
-        {
-            // Leaf area index for the subcanopy layer by index. Function of specific leaf weight SLWMAX and the depth of the canopy
-            // Depth of the canopy is expressed by the mass of foliage above this subcanopy layer (i.e. slwdel * index/imax *fol)
-            float LAISum = cumulativeLAI;
-            // Cohort LAI is capped at 25; once LAI reaches 25, subsequent sublayers get LAI of 0.01
-            float LAIlayerMax = (float)Math.Max(0.01, 25.0F - LAISum);
-            float LAIlayer = 1 / (float)Globals.IMAX * fol / (species.SLWmax - species.SLWDel * index * (1 / (float)Globals.IMAX) * fol);
-            if (fol > 0 && LAIlayer <= 0)
-            {
-                Globals.ModelCore.UI.WriteLine("\n Warning: LAI was calculated to be negative for " + species.Name + ". This could be caused by a low value for SLWmax.  LAI applied in this case is a max of 25 for each cohort.");
-                LAIlayer = LAIlayerMax / (Globals.IMAX - index);
-            }
-            else
-                LAIlayer = (float)Math.Min(LAIlayerMax, LAIlayer);
-            return LAIlayer;
         }
 
         /// <summary>

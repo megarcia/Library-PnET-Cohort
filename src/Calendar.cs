70a81dc1b49f6261e813fa9d31601323146731af
Matthew Garcia <matt.e.garcia@gmail.com>
2025-08-21 13:40:08 -0500

    fixed switch/case structure in CalcDaySpan function

--- src/Calendar.cs
+++ src/Calendar.cs
@@ -21,21 +21,21 @@ namespace Landis.Library.PnETCohorts
 
         public static int CalcDaySpan(int Month)
         {
-            return Month switch
+            switch (Month)
             {
-                1 => 31,
-                2 => 28,
-                3 => 31,
-                4 => 30,
-                5 => 31,
-                6 => 30,
-                7 => 31,
-                8 => 31,
-                9 => 30,
-                10 => 31,
-                11 => 30,
-                12 => 31,
-                _ => throw new System.Exception("Month " + Month + " is not an integer between 1-12. Error assigning DaySpan"),
+                case 1: return 31;
+                case 2: return 28;
+                case 3: return 31;
+                case 4: return 30;
+                case 5: return 31;
+                case 6: return 30;
+                case 7: return 31;
+                case 8: return 31;
+                case 9: return 30;
+                case 10: return 31;
+                case 11: return 30;
+                case 12: return 31;
+                default: throw new System.Exception("Month " + Month + " is not an integer between 1-12. Error assigning DaySpan");
             };
         }
 

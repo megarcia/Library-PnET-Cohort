d77b70983d4a937e35d3779bf178871211ac3e9b
Matthew Garcia <matt.e.garcia@gmail.com>
2026-05-10 15:21:21 -0500

    removed "using Microsoft.VisualBasic;" directive

--- src/Permafrost.cs
+++ src/Permafrost.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Microsoft.VisualBasic;
+
 
 namespace Landis.Library.PnETCohorts
 {
--- src/SiteCohorts.cs
+++ src/SiteCohorts.cs
@@ -13,7 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
-using Microsoft.VisualBasic;
+
 
 namespace Landis.Library.PnETCohorts
 {

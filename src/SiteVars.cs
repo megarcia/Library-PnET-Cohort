abaecd3bdb68b853a18508da98f0dadb71bbebb6
Matthew Garcia <matt.e.garcia@gmail.com>
2026-09-09 09:26:06 -0500

    testing -- corrected variable registration

--- src/SiteVars.cs
+++ src/SiteVars.cs
@@ -42,7 +42,7 @@ namespace Landis.Library.PnETCohorts
             MonthlySoilTemp = Globals.ModelCore.Landscape.NewSiteVar<SortedList<float, float>[]>();
             FieldCapacity = Globals.ModelCore.Landscape.NewSiteVar<float>();
 
-            Globals.ModelCore.RegisterSiteVar(SiteCohorts, "Succession.UniversalCohorts");  // MG 20260908
+            Globals.ModelCore.RegisterSiteVar(UniversalCohorts, "Succession.UniversalCohorts");  // MG20260908, 20260909
             Globals.ModelCore.RegisterSiteVar(WoodyDebris, "Succession.WoodyDebris");
             Globals.ModelCore.RegisterSiteVar(Litter, "Succession.Litter");
             Globals.ModelCore.RegisterSiteVar(FineFuels, "Succession.FineFuels");

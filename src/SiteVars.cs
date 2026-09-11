ff6cad9cf803e4f419e046eb304f8f188464d86d
Matthew Garcia <matt.e.garcia@gmail.com>
2026-09-11 11:27:27 -0500

    created UpdateUniversalCohorts function using commented code from Extension PlugIn.cs; trying to figue out how to keep Universal Cohorts updated to pass PnET Cohorts info to other Extensions (e.g. Dynamic Fuels)

--- src/SiteVars.cs
+++ src/SiteVars.cs
@@ -7,14 +7,13 @@ using Landis.SpatialModeling;
 
 namespace Landis.Library.PnETCohorts
 {
-
     public static class SiteVars
     {
         public static ISiteVar<SiteCohorts> SiteCohorts;
         public static ISiteVar<UniversalCohorts.SiteCohorts> UniversalCohorts;
         public static ISiteVar<Pool> WoodyDebris;
         public static ISiteVar<Pool> Litter;
-        public static ISiteVar<Double> FineFuels;
+        public static ISiteVar<double> FineFuels;
         public static ISiteVar<float> PressureHead;
         public static ISiteVar<float> ExtremeMinTemp;
         public static ISiteVar<double> AnnualPE;  //Annual Potential Evaporation
@@ -31,13 +30,13 @@ namespace Landis.Library.PnETCohorts
             UniversalCohorts = Globals.ModelCore.Landscape.NewSiteVar<UniversalCohorts.SiteCohorts>();
             WoodyDebris = Globals.ModelCore.Landscape.NewSiteVar<Pool>();
             Litter = Globals.ModelCore.Landscape.NewSiteVar<Pool>();
-            FineFuels = Globals.ModelCore.Landscape.NewSiteVar<Double>();
+            FineFuels = Globals.ModelCore.Landscape.NewSiteVar<double>();
             PressureHead = Globals.ModelCore.Landscape.NewSiteVar<float>();
             ExtremeMinTemp = Globals.ModelCore.Landscape.NewSiteVar<float>();
-            AnnualPE = Globals.ModelCore.Landscape.NewSiteVar<Double>();
-            ClimaticWaterDeficit = Globals.ModelCore.Landscape.NewSiteVar<Double>();
-            SmolderConsumption = Globals.ModelCore.Landscape.NewSiteVar<Double>();
-            FlamingConsumption = Globals.ModelCore.Landscape.NewSiteVar<Double>();
+            AnnualPE = Globals.ModelCore.Landscape.NewSiteVar<double>();
+            ClimaticWaterDeficit = Globals.ModelCore.Landscape.NewSiteVar<double>();
+            SmolderConsumption = Globals.ModelCore.Landscape.NewSiteVar<double>();
+            FlamingConsumption = Globals.ModelCore.Landscape.NewSiteVar<double>();
             MonthlyPressureHead = Globals.ModelCore.Landscape.NewSiteVar<float[]>();
             MonthlySoilTemp = Globals.ModelCore.Landscape.NewSiteVar<SortedList<float, float>[]>();
             FieldCapacity = Globals.ModelCore.Landscape.NewSiteVar<float>();
@@ -49,13 +48,31 @@ namespace Landis.Library.PnETCohorts
             Globals.ModelCore.RegisterSiteVar(FineFuels, "Succession.FineFuels");
             Globals.ModelCore.RegisterSiteVar(PressureHead, "Succession.PressureHead");
             Globals.ModelCore.RegisterSiteVar(ExtremeMinTemp, "Succession.ExtremeMinTemp");
-            Globals.ModelCore.RegisterSiteVar(AnnualPE, "Succession.PET"); //FIXME
+            Globals.ModelCore.RegisterSiteVar(AnnualPE, "Succession.PET"); //FIXME  //MG20260911 Fix what?
             Globals.ModelCore.RegisterSiteVar(ClimaticWaterDeficit, "Succession.CWD");
             Globals.ModelCore.RegisterSiteVar(SmolderConsumption, "Succession.SmolderConsumption");
             Globals.ModelCore.RegisterSiteVar(FlamingConsumption, "Succession.FlamingConsumption");
             Globals.ModelCore.RegisterSiteVar(MonthlyPressureHead, "Succession.MonthlyPressureHead");
             Globals.ModelCore.RegisterSiteVar(MonthlySoilTemp, "Succession.MonthlySoilTemp");
             Globals.ModelCore.RegisterSiteVar(FieldCapacity, "Succession.SoilFieldCapacity");
+        }
+
+        //MG20260911 moved here from PlugIn.cs and modified accordingly
+        public static void UpdateUniversalCohorts()
+        {
+            foreach (ActiveSite site in Globals.ModelCore.Landscape)
+            {
+                UniversalCohorts[site] = new Library.UniversalCohorts.SiteCohorts();
+                foreach(Library.UniversalCohorts.ISpeciesCohorts speciesCohort in SiteCohorts[site])
+                {
+                    foreach (Library.UniversalCohorts.ICohort cohort in speciesCohort)
+                        UniversalCohorts[site].AddNewCohort(cohort.Species, cohort.Data.Age,
+                                                            (int)cohort.Data.Biomass, cohort.Data.ANPP,
+                                                            cohort.Data.AdditionalParameters);
+                }
+                if (SiteCohorts[site] != null && UniversalCohorts[site] == null)
+                    throw new Exception("Cannot convert PnET SiteCohorts to Universal Cohorts");
+            }
         }
     }
 }

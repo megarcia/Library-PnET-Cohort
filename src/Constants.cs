9ac8cc0497f60a1e53046e63cb3f9185a1fc4e25
Matthew Garcia <matt.e.garcia@gmail.com>
2026-09-15 11:02:00 -0500

    explicitly declare all decimals as floats, instead of default doubles

--- src/Constants.cs
+++ src/Constants.cs
@@ -6,32 +6,32 @@ namespace Landis.Library.PnETCohorts
         /// <summary>
         /// Million
         /// </summary>
-        public const float Million = 1.0E6;
+        public const float Million = 1.0E6f;
 
         /// <summary>
         /// Billion
         /// </summary>
-        public const float Billion = 1.0E9;
+        public const float Billion = 1.0E9f;
 
         /// <summary>
         /// Seconds per hour
         /// </summary>
-        public const float SecondsPerHour = 60.0 * 60.0;
+        public const float SecondsPerHour = 60.0f * 60.0f;
 
         /// <summary>
         /// Seconds per day
         /// </summary>
-        public const float SecondsPerDay = SecondsPerHour * 24.0;
+        public const float SecondsPerDay = SecondsPerHour * 24.0f;
 
         /// <summary>
         /// Mass density of C in g/mol
         /// </summary>
-        public const float MC = 12.0;
+        public const float MC = 12.0f;
 
         /// <summary>
         /// Mass density of CO2 in g/mol
         /// </summary>
-        public const float MCO2 = 44.0;
+        public const float MCO2 = 44.0f;
 
         /// <summary>
         /// Ratio of mass density of CO2 to C 
@@ -41,152 +41,152 @@ namespace Landis.Library.PnETCohorts
         /// <summary>
         /// Joules <--> calorie conversion
         /// </summary>
-        public const float Jpercal = 4.184;
-        public const float CalperJ = 1.0 / Jpercal;
+        public const float Jpercal = 4.184f;
+        public const float CalperJ = 1.0f / Jpercal;
 
         /// <summary>
         /// Universal gas constant in J/kmol.K
         /// </summary>
-        public const float GasConst_JperkmolK = 8314.47;
+        public const float GasConst_JperkmolK = 8314.47f;
 
         /// <summary>
         /// Reference temperature in K
         /// </summary>
-        public const float Tref_K = 273.15;
+        public const float Tref_K = 273.15f;
 
         /// <summary>
         /// Reference pressure in kPa
         /// </summary>
-        public const float Pref_kPa = 101.3;
+        public const float Pref_kPa = 101.3f;
 
         /// <summary>
         /// Psychrometric coefficient in kPa/K
         /// (Cabrera et al., 2016, Table 1)
         /// </summary>
-        public const float PsychrometricCoeff = 0.062;
+        public const float PsychrometricCoeff = 0.062f;
 
         /// <summary>
         /// Reference atmospheric concentration of CO2 in ppm
         /// </summary>
-        public const float CO2RefConc = 350.0;
+        public const float CO2RefConc = 350.0f;
 
         /// <summary>
         /// convert kJ/m.d.K to W/m.K
         /// </summary>
-        public const float Convert_kJperday_to_Watts = 0.2777777778 / 24.0;
+        public const float Convert_kJperday_to_Watts = 0.2777777778f / 24.0f;
 
         /// <summary>
         /// density of water in kg/m3
         /// </summary>
-        public const float DensityWater = 1000.0;
+        public const float DensityWater = 1000.0f;
 
         /// <summary>
         /// latent heat of vaporization for water in MJ/m3
         /// </summary>
-        public const float LatentHeatVaporWater = 2453.0;
+        public const float LatentHeatVaporWater = 2453.0f;
 
         /// <summary>
         /// heat capacity of solid portion of soil in kJ/m3.K 
         /// (Farouki, 1986, in vanLier and Durigon, 2013)
         /// </summary>
-        public const float HeatCapacitySoil = 1942.0;
+        public const float HeatCapacitySoil = 1942.0f;
 
         /// <summary>
         /// heat capacity of water in kJ/m3.K = J/kg.K
         /// (vanLier and Durigon, 2013)
         /// </summary>
-        public const float HeatCapacityWater_Jperkg = 4186.0;
+        public const float HeatCapacityWater_Jperkg = 4186.0f;
 
         /// <summary>
         /// heat capacity of snow in J/kg.K 
         /// (https://www.engineeringtoolbox.com/specific-heat-capacity-d_391.html)
         /// </summary>
-        public const float HeatCapacitySnow_Jperkg = 2090.0;
+        public const float HeatCapacitySnow_Jperkg = 2090.0f;
 
         /// <summary>
         /// heat capacity of moss in kJ/m3.K 
         /// (Sazonova and Romanovsky, 2003)
         /// </summary>
-        public const float HeatCapacityMoss = 2500.0;
+        public const float HeatCapacityMoss = 2500.0f;
 
         /// <summary>
         /// thermal conductivity of air in kJ/m.d.K 
         /// (vanLier and Durigon, 2013)
         /// </summary>
-        public const float ThermalConductivityAir_kJperday = 2.25;
+        public const float ThermalConductivityAir_kJperday = 2.25f;
 
         /// <summary>
         /// thermal conductivity of air in W/m.K 
         /// (CLM5 documentation, Table 2.7)
         /// </summary>
-        public const float ThermalConductivityAir_Watts = 0.023;
+        public const float ThermalConductivityAir_Watts = 0.023f;
 
         /// <summary>
         /// thermal conductivity of water in kJ/m.d.K
         /// (vanLier and Durigon, 2013)
         /// </summary>
-        public const float ThermalConductivityWater_kJperday = 51.51;
+        public const float ThermalConductivityWater_kJperday = 51.51f;
 
         /// <summary>
         /// thermal conductivity of water in W/m.K 
         /// (CLM5 documentation, Table 2.7)
         /// </summary>
-        public const float ThermalConductivityWater_Watts = 0.57;
+        public const float ThermalConductivityWater_Watts = 0.57f;
 
         /// <summary>
         /// thermal conductivity of ice in W/m.K 
         /// (CLM5 documentation, Table 2.7)
         /// </summary>
-        public const float ThermalConductivityIce_Watts = 2.29;
+        public const float ThermalConductivityIce_Watts = 2.29f;
 
         /// <summary>
         /// thermal conductivity of clay soil in kJ/m.d.K
         /// (Michot et al., 2008 in vanLier and Durigon, 2013)
         /// </summary>
-        public const float ThermalConductivityClay = 80.0;
+        public const float ThermalConductivityClay = 80.0f;
 
         /// <summary>
         /// thermal conductivity of sandstone in kJ/m.d.K 
         /// (Gemant, 1950, in vanLier and Durigon, 2013)
         /// </summary>
-        public const float ThermalConductivitySandstone = 360.0;
+        public const float ThermalConductivitySandstone = 360.0f;
 
         /// <summary>
         /// thermal conductivity of moss in kJ/m.d.K
         /// converted from 0.2 W/m.K (Sazonova and Romanovsky, 2003)
         /// </summary>
-        public const float ThermalConductivityMoss = 432.0;
+        public const float ThermalConductivityMoss = 432.0f;
 
         /// <summary>
         /// unexplained coefficient in vanLier and Durigon (2013)
         /// (via Farouki, 1986)
         /// </summary>
-        public const float gs = 0.125;
+        public const float gs = 0.125f;
 
         /// <summary>
         /// angular velocity of Earth in radians/day
         /// </summary>
-        public const float omega = 2.0 * (float)Math.PI;
+        public const float omega = 2.0f * (float)Math.PI;
 
         /// <summary>
         /// length of temperature record in months
         /// </summary>
-        public const float tau = 12.0;
+        public const float tau = 12.0f;
 
         /// <summary>
         /// intercept of function for bulk density of snow in kg/m3
         /// </summary>
-        public const float DensitySnow_intercept = 165.0;
+        public const float DensitySnow_intercept = 165.0f;
 
         /// <summary>
         /// slope of function for bulk density of snow in kg/m3
         /// </summary>
-        public const float DensitySnow_slope = 1.3;
+        public const float DensitySnow_slope = 1.3f;
 
         /// <summary>
         /// minimum depth of snow (m) that counts as 
         /// full snow cover for albedo calculations
         /// </summary>
-        public const float SnowReflectanceThreshold = 0.1;
+        public const float SnowReflectanceThreshold = 0.1f;
     }
 }

c706a91e83f69dc8f6332fe0aa55eb21045858a5
Matthew Garcia <matt.e.garcia@gmail.com>
2026-09-15 10:48:37 -0500

    moved physical constants to Constants.cs; minor cleanup

--- src/utility/Globals.cs
+++ src/utility/Globals.cs
@@ -1,9 +1,10 @@
-﻿using Landis.Core;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using Landis.Core;
+using Landis.Climate;
 
 namespace Landis.Library.PnETCohorts
 {
@@ -23,13 +24,6 @@ namespace Landis.Library.PnETCohorts
         public static readonly object distributionThreadLock = new object();
         public static readonly object ecoregionDataThreadLock = new object();
         public static readonly object initialSitesThreadLock = new object();
-        public const float bulkIntercept = 165.0f; //kg/m3
-        public const float bulkSlope = 1.3f; //kg/m3
-        public const float Pwater = 1000.0f;  // Density of water (kg/m3)
-        public const float lambAir = 0.023f; // W/m K (CLM5 documentation, Table 2.7)
-        public const float lambIce = 2.29f; // W/m K (CLM5 documentation, Table 2.7)
-        public const float snowHeatCapacity = 2090f; // J/kg K (https://www.engineeringtoolbox.com/specific-heat-capacity-d_391.html)
-        public const float snowReflectanceThreshold = 0.100f;  // minimum depth of snow (m) that counts as full snow for albedo calculations
 
         public static void InitializeCore(ICore mCore, ushort IMAX, DateTime startDate)
         {
@@ -59,7 +53,6 @@ namespace Landis.Library.PnETCohorts
         public static int ConvertYearToFutureClimateYear(DateTime date)
         {
             int convert = date.Year - MinFutureClimateYear + 1;
-
             return convert >= 1 ? convert : -1;
         }
 
@@ -72,11 +65,9 @@ namespace Landis.Library.PnETCohorts
             {
                 if (year < MinSpinUpClimateYear)
                     year += Climate.Climate.SpinupAvailableYearCount;
-
                 if (year > MinSpinUpClimateYear + Climate.Climate.SpinupAvailableYearCount)
                     year -= Climate.Climate.SpinupAvailableYearCount;
             }
-
             var convert = year - MinSpinUpClimateYear + 1;
             return convert;
         }

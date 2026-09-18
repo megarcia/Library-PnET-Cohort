ee4bf4b61da745b21df8aa5364a62471f7588e6d
Matthew Garcia <matt.e.garcia@gmail.com>
2026-09-18 14:42:13 -0500

    minor cleanup

--- src/utility/Statistics.cs
+++ src/utility/Statistics.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+//using System.Collections.Generic;
+//using System.Text;
 
 namespace Landis.Library.PnETCohorts
 {
@@ -9,39 +9,36 @@ namespace Landis.Library.PnETCohorts
         /// <summary>
         /// Choose random integer between min and max (inclusive)
         /// </summary>
-        /// <param name="min">Minimum integer</param>
-        /// <param name="max">Maximum integer</param>
+        /// <param name="min">range minimum, integer</param>
+        /// <param name="max">range maximum, integer</param>
         /// <returns></returns>
         public static int DiscreteUniformRandom(int min, int max)
         {
             lock (Globals.distributionThreadLock)
             {
-                Globals.ModelCore.ContinuousUniformDistribution.Alpha = 0;
-                Globals.ModelCore.ContinuousUniformDistribution.Beta = max + 1;
                 Globals.ModelCore.ContinuousUniformDistribution.Alpha = min;
+                Globals.ModelCore.ContinuousUniformDistribution.Beta = max + 1;
                 Globals.ModelCore.ContinuousUniformDistribution.NextDouble();
-
-                //double testMin = ModelCore.ContinuousUniformDistribution.Minimum;
-                //double testMax = ModelCore.ContinuousUniformDistribution.Maximum;
-
                 double valueD = Globals.ModelCore.ContinuousUniformDistribution.NextDouble();
                 int value = Math.Min((int)valueD, max);
-
                 return value;
             }
         }
 
+        /// <summary>
+        /// Choose random double between min and max (inclusive)
+        /// </summary>
+        /// <param name="min">range minimum, double, default 0</param>
+        /// <param name="max">range maximum, double, default 1</param>
+        /// <returns></returns>
         public static double ContinuousUniformRandom(double min = 0, double max = 1)
         {
             lock (Globals.distributionThreadLock)
             {
-                Globals.ModelCore.ContinuousUniformDistribution.Alpha = 0;
-                Globals.ModelCore.ContinuousUniformDistribution.Beta = max;
                 Globals.ModelCore.ContinuousUniformDistribution.Alpha = min;
+                Globals.ModelCore.ContinuousUniformDistribution.Beta = max;
                 Globals.ModelCore.ContinuousUniformDistribution.NextDouble();
-
                 double value = Globals.ModelCore.ContinuousUniformDistribution.NextDouble();
-
                 return value;
             }
         }
